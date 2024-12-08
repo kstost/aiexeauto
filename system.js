2e6d9b30b4f1b1b7f87bff34a46c37d8bc133cf3
kst <kstost@gmail.com>
2024-12-08 18:42:50 +0900

    Update system.js

--- system.js
+++ system.js
@@ -40,7 +40,7 @@ export async function loadConfiguration() {
         llm: "claude",
         maxIterations: 0,
         dockerImage: 'my-node-ubuntu',
-        useDocker: false, // Docker 사용 여부 (false: 도커 아닌 웹컨테이너 사용, true: 도커 사용함) 윈도우즈에서는 도커 지원하지 않음
+        useDocker: true, // Docker 사용 여부 (false: 도커 아닌 웹컨테이너 사용, true: 도커 사용함)
         dockerWorkDir: '/home/ubuntu/work',
         overwriteOutputDir: false, // 덮어쓰기 여부 (false: 덮어쓰지 않음, true: 덮어씀)
     }
